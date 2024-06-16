6d53467498540aca80ca50044dfba01284bd113b
arruta <evgeni.v.genov@gmail.com>
2024-06-17 00:17:01 +0300

    Initial commit

---
+++ src/main/java/com/dictionaryapp/model/entity/Language.java
@@ -0,0 +1,61 @@
+package com.dictionaryapp.model.entity;
+
+import com.dictionaryapp.model.enums.LanguageEnum;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "language")
+public class Language {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private LanguageEnum languageName;
+
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "language")
+    private Set<Word> words;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Language setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public LanguageEnum getLanguageName() {
+        return languageName;
+    }
+
+    public Language setLanguageName(LanguageEnum languageName) {
+        this.languageName = languageName;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Language setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public Set<Word> getWords() {
+        return words;
+    }
+
+    public Language setWords(Set<Word> words) {
+        this.words = words;
+        return this;
+    }
+}
